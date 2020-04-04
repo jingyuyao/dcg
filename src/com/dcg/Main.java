0a0ee61591fe7d0f8fd4c0fa0d72eebac508c92c
Jingyu Yao <yjy.emails@gmail.com>
2020-04-04 14:18:55 -0700

    input loop

--- src/com/dcg/Main.java
+++ src/com/dcg/Main.java
@@ -1,8 +1,17 @@
 package com.dcg;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String userCommand = scanner.nextLine();
+            if (userCommand.equals("quit")) {
+                System.out.println("goodbye");
+                break;
+            }
+        }
     }
 }
