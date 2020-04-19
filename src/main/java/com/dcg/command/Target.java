e614356dcc1c36565b6f49f255ea20b1ecb3cca2
Jingyu Yao <yjy.emails@gmail.com>
2020-04-19 10:35:18 -0700

    nit

--- src/main/java/com/dcg/command/Target.java
+++ src/main/java/com/dcg/command/Target.java
@@ -3,5 +3,6 @@ package com.dcg.command;
 import java.util.List;
 import java.util.function.Supplier;
 
+// TODO: change this into something with getFrom() and getTo()
 /** Marker type for command targets. */
 public interface Target extends Supplier<List<Integer>> {}
--- src/main/java/com/dcg/source/EffectSource.java
+++ src/main/java/com/dcg/source/EffectSource.java
@@ -3,6 +3,7 @@ package com.dcg.source;
 import com.dcg.command.Target;
 import java.util.function.Supplier;
 
+// TODO: change this into something with getCount() and getValue()
 /**
  * An additional source of target for the command. Result will be automatically memorized by the
  * command. Instances will be injected.
