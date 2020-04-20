4ea17e9e45421257a203c710962afe73a158a08e
Jingyu Yao <yjy.emails@gmail.com>
2020-04-19 17:32:40 -0700

    initial game server setup

--- src/main/java/com/dcg/Main.java
+++ src/main/java/com/dcg/Main.java
@@ -1,37 +1,15 @@
 package com.dcg;
 
-import com.dcg.game.Game;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
-import java.util.Scanner;
-import java.util.stream.Collectors;
+import com.dcg.game.GameServer;
+import java.net.InetSocketAddress;
+import org.java_websocket.server.WebSocketServer;
 
 public class Main {
+  private static final String HOST = "localhost";
+  private static final int PORT = 8888;
 
   public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    Game game = new Game();
-    while (!game.isOver()) {
-      System.out.print("> ");
-      String input = scanner.nextLine();
-      Optional<List<Integer>> message = parse(input);
-      if (message.isPresent()) {
-        game.handleInput(message.get());
-      } else {
-        System.out.printf("Unable to parse input: %s\n", input);
-      }
-    }
-    System.out.println("goodbye");
-  }
-
-  private static Optional<List<Integer>> parse(String input) {
-    try {
-      String[] tokenized = input.split(" ");
-      return Optional.of(
-          Arrays.stream(tokenized).map(Integer::parseInt).collect(Collectors.toList()));
-    } catch (RuntimeException e) {
-      return Optional.empty();
-    }
+    WebSocketServer server = new GameServer(new InetSocketAddress(HOST, PORT));
+    server.run();
   }
 }
---
+++ src/main/java/com/dcg/game/GameServer.java
@@ -0,0 +1,82 @@
+package com.dcg.game;
+
+import java.net.InetSocketAddress;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import org.java_websocket.WebSocket;
+import org.java_websocket.handshake.ClientHandshake;
+import org.java_websocket.server.WebSocketServer;
+
+public class GameServer extends WebSocketServer {
+  private Game game = new Game();
+
+  public GameServer(InetSocketAddress address) {
+    super(address);
+  }
+
+  @Override
+  public void onOpen(WebSocket conn, ClientHandshake handshake) {
+    System.out.println("New connection: " + conn.getRemoteSocketAddress());
+  }
+
+  @Override
+  public void onClose(WebSocket conn, int code, String reason, boolean remote) {
+    System.out.println("Connection closed: " + conn.getRemoteSocketAddress());
+  }
+
+  @Override
+  public void onMessage(WebSocket conn, String message) {
+    System.out.println("New message: " + message);
+
+    List<String> tokens = Arrays.asList(message.replace("\n", "").split(" "));
+    if (tokens.isEmpty()) {
+      System.out.println("No tokens");
+      return;
+    }
+    switch (tokens.get(0)) {
+      case "execute":
+        if (tokens.size() > 1) {
+          List<String> rawArguments = tokens.subList(1, tokens.size());
+          Optional<List<Integer>> arguments = parse(rawArguments);
+          if (arguments.isPresent()) {
+            game.handleInput(arguments.get());
+            if (game.isOver()) {
+              System.out.println("GG");
+              game = new Game();
+            }
+          } else {
+            System.out.println("Unable to parse arguments: " + rawArguments);
+          }
+        } else {
+          System.out.println("execute requires arguments");
+        }
+        break;
+      case "query":
+        // TODO: broadcast something
+        break;
+      default:
+        System.out.println("Unknown " + message);
+        break;
+    }
+  }
+
+  @Override
+  public void onError(WebSocket conn, Exception ex) {
+    System.err.println("New connection: " + conn.getRemoteSocketAddress());
+  }
+
+  @Override
+  public void onStart() {
+    System.err.println("Server started");
+  }
+
+  private static Optional<List<Integer>> parse(List<String> arguments) {
+    try {
+      return Optional.of(arguments.stream().map(Integer::parseInt).collect(Collectors.toList()));
+    } catch (RuntimeException e) {
+      return Optional.empty();
+    }
+  }
+}
