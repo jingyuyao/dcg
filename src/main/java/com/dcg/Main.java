4c58a7e2670eef03ef0d87654058e9863655daba
Jingyu Yao <yjy.emails@gmail.com>
2020-04-21 22:25:23 -0700

    force reuse socket

--- src/main/java/com/dcg/Main.java
+++ src/main/java/com/dcg/Main.java
@@ -10,6 +10,7 @@ public class Main {
 
   public static void main(String[] args) {
     WebSocketServer server = new GameServer(new InetSocketAddress(HOST, PORT));
+    server.setReuseAddr(true);
     server.run();
   }
 }
