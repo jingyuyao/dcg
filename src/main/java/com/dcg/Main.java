8ee9ba121889f0f526e9e94daca0eab4406c0c0c
Jingyu Yao <yjy.emails@gmail.com>
2020-05-02 13:23:47 -0700

    prod port choosing

--- src/main/java/com/dcg/Main.java
+++ src/main/java/com/dcg/Main.java
@@ -6,10 +6,12 @@ import org.java_websocket.server.WebSocketServer;
 
 public class Main {
   private static final String HOST = "0.0.0.0";
-  private static final int PORT = 8888;
+  private static final int DEBUG_PORT = 8888;
+  private static final int PROD_PORT = 80;
 
   public static void main(String[] args) {
-    WebSocketServer server = new GameServer(new InetSocketAddress(HOST, PORT));
+    int port = args.length > 0 && "prod".equals(args[0]) ? PROD_PORT : DEBUG_PORT;
+    WebSocketServer server = new GameServer(new InetSocketAddress(HOST, port));
     server.setReuseAddr(true);
     server.run();
   }
