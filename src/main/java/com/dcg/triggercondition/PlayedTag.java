b43f1c4d1d73232cec708944278e81ce70dd0253
Jingyu Yao <yjy.emails@gmail.com>
2020-05-03 10:48:30 -0700

    fix color checking bug
    unit owner is now player and not card

--- src/main/java/com/dcg/triggercondition/PlayedTag.java
+++ src/main/java/com/dcg/triggercondition/PlayedTag.java
@@ -2,6 +2,8 @@ package com.dcg.triggercondition;
 
 import com.artemis.Aspect;
 import com.artemis.Component;
+import com.artemis.ComponentMapper;
+import com.dcg.battle.Unit;
 import com.dcg.game.CoreSystem;
 import com.dcg.location.PlayArea;
 import java.util.List;
@@ -13,6 +15,7 @@ import java.util.List;
 public class PlayedTag implements TriggerCondition {
   private final Class<? extends Component> tag;
   protected CoreSystem coreSystem;
+  protected ComponentMapper<Unit> mUnit;
 
   public PlayedTag(Class<? extends Component> tag) {
     this.tag = tag;
@@ -20,10 +23,10 @@ public class PlayedTag implements TriggerCondition {
 
   @Override
   public boolean test(int originEntity, List<Integer> allowedTargets) {
+    int originCardEntity =
+        mUnit.has(originEntity) ? mUnit.get(originEntity).cardEntity : originEntity;
     return coreSystem
         .getStream(Aspect.all(PlayArea.class, tag))
-        .anyMatch(
-            cardEntity ->
-                cardEntity != originEntity && !coreSystem.isOwnedBy(cardEntity, originEntity));
+        .anyMatch(cardEntity -> cardEntity != originCardEntity);
   }
 }
